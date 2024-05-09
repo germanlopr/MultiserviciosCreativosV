91cf2aee758c7e3436f414bf929c65b31abe8e4b
germanlopr <ge5lo@hotmail.com>
2024-05-09 15:56:28 -0500

    boton arriba sin home o casita

--- js/script.js
+++ js/script.js
@@ -1,35 +1,13 @@
-// window.addEventListener('scroll', function() {
-//     var sections = document.querySelectorAll('.section');
-
-//     for (var i = 0; i < sections.length; i++) {
-//       var section = sections[i];
-//       var sectionTop = section.getBoundingClientRect().top;
-//       var sectionBottom = section.getBoundingClientRect().bottom;
-
-//       if (sectionTop < window.innerHeight && sectionBottom >= 0) {
-//         var textCard = section.querySelector('.text-card');
-//         var imageCard = section.querySelector('.image-placeholder');
-
-//         textCard.classList.add('animate__animated', 'animate__fadeInLeft');
-//         imageCard.classList.add('animate__animated', 'animate__fadeInRight');
-//       }
-//     }
-//   });
-
-window.addEventListener('scroll', function() {
-    var sections = document.querySelectorAll('.section');
-  
-    sections.forEach(section => {
-      var sectionTop = section.getBoundingClientRect().top;
-      var sectionBottom = section.getBoundingClientRect().bottom;
-  
-      if (sectionTop < window.innerHeight && sectionBottom >= 0) {
-        var textCard = section.querySelector('.text-card');
-        var imageCard = section.querySelector('.image-placeholder');
-  
-        textCard.classList.add('animate-left');
-        imageCard.classList.add('animate-right');
+// Función para mostrar el botón después de desplazarse hacia abajo 100px
+window.onscroll = function() {
+    var boton = document.querySelector(".home-btn")
+    console.log(boton)
+    // Asegúrate de que el elemento exista
+    if (boton) {
+      if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        boton.classList.add('activo');
+      } else {
+        boton.classList.remove('activo');
       }
-    });
-  });
-  
+    }
+  };
